d11563a2c3707a9075635cae3a6a2213883e9b57
Ifeanyi Anthony Omeifejideofor <kifeanyi66@gmail.com>
2019-11-21 16:36:26 +0100

    adding starting projects

---
+++ script.py
@@ -0,0 +1,24 @@
+from translate import Translator
+
+translator= Translator(to_lang="pt")
+try:
+	with open ('./test.txt', mode = 'r') as file:
+		text = file.read()
+		translation = translator.translate(text)
+		with open ('./testa.txt', mode = 'w') as file2:
+			file2.write(translation)
+except TabError as c:
+	print('check your spaces')
+	
+	
+	
+
+	
+ 
+
+
+
+
+
+
+
